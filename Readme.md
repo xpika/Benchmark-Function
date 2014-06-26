e3d0d2f79c8aa2e6726e5c0ba05d18dbde2138f3
alan hawkins <hawk.alan@gmail.com>
2014-06-26 13:59:19 +1000

    fixed example

--- Readme.md
+++ Readme.md
@@ -1,17 +1,21 @@
 ```
 "initialising data"
 "initialising Data.List"
+"initialising Data.List of Ints"
 "initialising Data.Vector"
+"initialising Data.Bimap"
 "initialising Data.RandomAccessList"
 "initialising Data.Sequence"
 "finish initialising data"
-testing Data.List.(!!) 
-0.356282
-testing Data.Vector.(!) 
-0.059852
-testing Data.RandomAcessList.index 
-2.19884
+testing Prelude.!! 
+0.452955
+testing Data.Vector.! 
+0.058301
+testing Data.Bimap.! 
+0.171011
+testing Data.RandomAcessList.lookup 
+0.077505
 testing Data.Sequence.index
-0.064547
+0.064149
 
 ```
--- TestList.hs
+++ TestList.hs
@@ -1,4 +1,7 @@
 
+{-# LANGUAGE FlexibleInstances #-}
+
+
 import TimeFunction
 import Data.RandomAccessList
 import Control.Monad
@@ -6,32 +9,47 @@ import Data.Vector
 import Data.Sequence
 import Control.Applicative
 import Data.Monoid
+import Data.Bimap
+import Control.Arrow
 
+monoidFromApplicativeList xs = monoidFromList (Prelude.map pure xs) 
 
-makeMonoid xs = Prelude.foldl mappend mempty (Prelude.map pure xs) 
+monoidFromList xs = Prelude.foldl mappend mempty xs
 
 list = [1..10000]
 
+instance Monoid (Bimap Int Integer) where
+  mempty = Data.Bimap.empty
+  mappend bm1 bm2 = Prelude.foldl (\subTotal (a,b) -> Data.Bimap.insert a b subTotal) bm1 (Data.Bimap.assocs bm2)
+
 main = do 
   print "initialising data"
-  let list' = list
+  let list' =  list
   print "initialising Data.List"
   runFunction list'
+  print "initialising Data.List of Ints"
+  let listInts = Prelude.map fromIntegral list'
+  runFunction listInts
   print "initialising Data.Vector"
-  let vector = makeMonoid (Prelude.map fromIntegral list) :: Vector Integer
+  let vector = monoidFromApplicativeList (Prelude.map fromIntegral list) :: Vector Integer
   runFunction vector
+  print "initialising Data.Bimap"
+  let bimap = monoidFromList (Prelude.map (fromIntegral >>= Data.Bimap.singleton) list)  :: Bimap Int Integer
+  runFunction bimap
   print "initialising Data.RandomAccessList"
-  let randomAccessList = Data.RandomAccessList.fromList list
+  let randomAccessList = Data.RandomAccessList.fromList list'
   runFunction randomAccessList
   print "initialising Data.Sequence"
-  let sequen = makeMonoid (Prelude.map fromIntegral list) :: Seq Integer
+  let sequen = monoidFromApplicativeList (Prelude.map fromIntegral list) :: Seq Integer
   runFunction sequen
   print "finish initialising data"
-  putStrLn "testing Data.List.(!!) " 
-  timeAndPrintAction (Control.Monad.forM list' (\x -> runFunction (list' !! (x-1)) ))
-  putStrLn "testing Data.Vector.(!) " 
-  timeAndPrintAction ((Control.Monad.forM list' (\x -> runFunction (vector ! (x-1)))))
-  putStrLn "testing Data.RandomAcessList.index " 
-  timeAndPrintAction ((Control.Monad.forM list' (\x -> runFunction ( Data.RandomAccessList.index (x-1) randomAccessList))))
+  putStrLn "testing Prelude.!! " 
+  timeAndPrintAction (Control.Monad.forM listInts (\x -> runFunction ((Prelude.!!) list' (x-1))))
+  putStrLn "testing Data.Vector.! " 
+  timeAndPrintAction (Control.Monad.forM listInts (\x -> runFunction ((Data.Vector.!) vector (x-1))))
+  putStrLn "testing Data.Bimap.! " 
+  timeAndPrintAction (Control.Monad.forM listInts (\x -> runFunction ((Data.Bimap.!) bimap x)))
+  putStrLn "testing Data.RandomAcessList.lookup " 
+  timeAndPrintAction ((Control.Monad.forM listInts (\x -> runFunction (Data.RandomAccessList.lookup (x-1) randomAccessList))))
   putStrLn "testing Data.Sequence.index"
-  timeAndPrintAction ((Control.Monad.forM list' (\x -> runFunction (Data.Sequence.index sequen (x-1)))))
+  timeAndPrintAction ((Control.Monad.forM listInts (\x -> runFunction (Data.Sequence.index sequen (x-1)))))
